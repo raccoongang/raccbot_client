cea02dc51c0447bc9d2b0c326155d836e689129f
Maksim Sokolskiy <misokolsky@gmail.com>
2016-01-30 15:41:32 +0200

    trying to add course detail view

--- raccbot_client/api.py
+++ raccbot_client/api.py
@@ -1,5 +1,7 @@
 import logging
 
+from django.contrib.auth.models import User
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from django.utils.decorators import method_decorator
@@ -31,6 +33,7 @@ from openedx.core.lib.api.authentication import (
 from openedx.core.lib.api.permissions import ApiKeyHeaderPermission, ApiKeyHeaderPermissionIsAuthenticated
 
 from enrollment import api
+from forms import CourseDetailGetForm
 
 
 log = logging.getLogger(__name__)
@@ -97,3 +100,132 @@ class CourseList(CourseListMixin, ListAPIView):
               value is null.
     """
     serializer_class = serializers.CourseSerializer
+
+
+
+from django.core.exceptions import ValidationError
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+
+from openedx.core.lib.api.paginators import NamespacedPageNumberPagination
+from openedx.core.lib.api.view_utils import view_auth_classes, DeveloperErrorViewMixin
+from .serializers import CourseDetailSerializer
+
+
+@view_auth_classes(is_authenticated=False)
+class CourseDetailView(DeveloperErrorViewMixin, RetrieveAPIView):
+    """
+    **Use Cases**
+
+        Request information on a course
+
+    **Example Requests**
+
+        GET /api/courses/v1/courses/{course_key}/
+
+    **Response Values**
+
+        Body consists of the following fields:
+
+        * blocks_url: Used to fetch the course blocks
+        * effort: A textual description of the weekly hours of effort expected
+            in the course.
+        * end: Date the course ends
+        * enrollment_end: Date enrollment ends
+        * enrollment_start: Date enrollment begins
+        * id: A unique identifier of the course; a serialized representation
+            of the opaque key identifying the course.
+        * media: An object that contains named media items.  Included here:
+            * course_image: An image to show for the course.  Represented
+              as an object with the following fields:
+                * uri: The location of the image
+        * name: Name of the course
+        * number: Catalog number of the course
+        * org: Name of the organization that owns the course
+        * overview: A possibly verbose HTML textual description of the course.
+            Note: this field is only included in the Course Detail view, not
+            the Course List view.
+        * short_description: A textual description of the course
+        * start: Date the course begins
+        * start_display: Readably formatted start of the course
+        * start_type: Hint describing how `start_display` is set. One of:
+            * `"string"`: manually set
+            * `"timestamp"`: generated form `start` timestamp
+            * `"empty"`: the start date should not be shown
+
+        Deprecated fields:
+
+        * course_id: Course key (use 'id' instead)
+
+    **Parameters:**
+
+        username (optional):
+            The username of the specified user for whom the course data
+            is being accessed. The username is not only required if the API is
+            requested by an Anonymous user.
+
+    **Returns**
+
+        * 200 on success with above fields.
+        * 400 if an invalid parameter was sent or the username was not provided
+          for an authenticated request.
+        * 403 if a user who does not have permission to masquerade as
+          another user specifies a username other than their own.
+        * 404 if the course is not available or cannot be seen.
+
+        Example response:
+
+            {
+                "blocks_url": "/api/courses/v1/blocks/?course_id=edX%2Fexample%2F2012_Fall",
+                "media": {
+                    "course_image": {
+                        "uri": "/c4x/edX/example/asset/just_a_test.jpg",
+                        "name": "Course Image"
+                    }
+                },
+                "description": "An example course.",
+                "end": "2015-09-19T18:00:00Z",
+                "enrollment_end": "2015-07-15T00:00:00Z",
+                "enrollment_start": "2015-06-15T00:00:00Z",
+                "course_id": "edX/example/2012_Fall",
+                "name": "Example Course",
+                "number": "example",
+                "org": "edX",
+                "overview: "<p>A verbose description of the course.</p>"
+                "start": "2015-07-17T12:00:00Z",
+                "start_display": "July 17, 2015",
+                "start_type": "timestamp"
+            }
+    """
+
+    serializer_class = CourseDetailSerializer
+
+    def get_object(self):
+        """
+        Return the requested course object, if the user has appropriate
+        permissions.
+        """
+        user = User.objects.get(username='maxsocl')
+        requested_params = self.request.query_params.copy()
+        requested_params.update({'course_key': self.kwargs['course_key_string']})
+
+        form = CourseDetailGetForm(requested_params, initial={'requesting_user': user})
+        if not form.is_valid():
+            raise ValidationError(form.errors)
+
+        return course_detail(
+            user,
+            form.cleaned_data['course_key'],
+        )
+
+
+from lms.djangoapps.courseware.courses import (
+    get_course_overview_with_access,
+    get_permission_for_course_about,
+)
+
+def course_detail(user, course_key):
+    return get_course_overview_with_access(
+        user,
+        get_permission_for_course_about(),
+        course_key,
+    )
--- raccbot_client/lms_urls.py
+++ raccbot_client/lms_urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, patterns, url
 from raccbot_client import api
 
+from django.conf import settings
+
 
 urlpatterns = patterns(
     '', url(r'', include('lms.urls')),
     url(r'^api/bot/courses/$', api.CourseList.as_view()),
+    url(r'^api/bot/courses/{}'.format(settings.COURSE_KEY_PATTERN), api.CourseDetailView.as_view(), name="course-detail"),
 )
